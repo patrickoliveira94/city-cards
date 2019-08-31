f2dd29bf662a29c854afd2025580e9e38c02a036
Patrick Mendonça Oliveira <oliveira.patrick94@gmail.com>
2019-08-30 22:57:12 -0300

    add gulp compress images

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@
 
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const image = require('gulp-image');
 
 sass.compiler = require("node-sass");
 
 gulp.task('default', watch);
-
 gulp.task("sass", compilaSass);
+gulp.task("image", compressImage);
 
 function compilaSass() {
   return gulp
@@ -16,6 +17,14 @@ function compilaSass() {
     .pipe(gulp.dest("assets/css"));
 }
 
+function compressImage() {
+  return gulp
+    .src('src/images/*')
+    .pipe(image())
+    .pipe(gulp.dest('assets/images'));
+}
+
 function watch() {
   gulp.watch("src/sass/*.sass", compilaSass);
+  gulp.watch("src/images/*", compressImage);
 }
